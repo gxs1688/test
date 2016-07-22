4bbee95a03e21b5b58c33479d99e1818a17e85c4
gxs1688 <gxs1688@sina.com>
2016-07-22 14:19:53 +0800

    Update User.java

--- src/com/pojo/User.java
+++ src/com/pojo/User.java
@@ -6,9 +6,11 @@ public class User {
 	private String pwd;
 
 	private String pwd66666; // 王宁+
+	private String pwd66668; // 郭帅+
 	private String pwd66667; // 王宁+
+	private String pwd66669; // 郭帅+
 	
 	private String pwd2;
-	private String pwd3;
 	private String pwd4;
+	private String pwd5;//郭帅
 }
