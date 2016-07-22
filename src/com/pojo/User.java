c105029a9998b16ca49768a2a23100926fdfd93c
WangNing <WangNinged@163.com>
2016-07-22 13:52:27 +0800

    update commit

--- src/com/pojo/User.java
+++ src/com/pojo/User.java
@@ -4,4 +4,11 @@ public class User {
 	private int id;
 	private String name;
 	private String pwd;
+
+	private String pwd66666; // 王宁+
+	private String pwd66667; // 王宁+
+	
+	private String pwd2;
+	private String pwd3;
+	private String pwd4;
 }
