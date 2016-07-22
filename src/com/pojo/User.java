fc37026dd7ecd4d41de71d551a5885b8558c6df4
WangNing <WangNinged@163.com>
2016-07-22 14:24:30 +0800

    update wangning User.java

--- src/com/pojo/User.java
+++ src/com/pojo/User.java
@@ -1,14 +1,17 @@
 package com.pojo;
 
 public class User {
-	private int id;
-	private String name;
-	private String pwd;
+    private int id;
+    private String name;
+    private String pwd;
 
-	private String pwd66666; // 王宁+
-	private String pwd66667; // 王宁+
-	
-	private String pwd2;
-	private String pwd3;
-	private String pwd4;
+    private String pwd66666; // 王宁+
+    private String pwd66667; // 王宁+
+
+    private String pwd2;
+    private String pwd3;
+    private String pwd4;
+
+    private String name$haha; //王宁+
+    private String pwd6667; //王宁+
 }
